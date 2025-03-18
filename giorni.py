e7faa51cd3957e32292cc8c725c55a9f2ccd23f0
marCatozzi <maryuricatozzi@gmail.com>
2025-03-18 16:15:54 +0100

    Update giorni.py

--- giorni.py
+++ giorni.py
@@ -1,13 +1,13 @@
 #Scrivete un programma per tradurre i numeri da 1 a 7 nei nomi dei giorni della settimana corrispondenti in Italiano.
 
 giorni=(
-"lunedì   " 
-"martedì  " 
-"mercoledì" 
-"giovedì  " 
-"venerdì  " 
-"sabato   " 
-"domenica "  
+"lunedì   " + 
+"martedì  " + 
+"mercoledì" + 
+"giovedì  " + 
+"venerdì  " + 
+"sabato   " + 
+"domenica "   
 )
 
 giorno = int(input("Indica giorno: "))
