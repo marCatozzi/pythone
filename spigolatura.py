1c5d93b5aa35d2b567331c16c11786d448ec4942
marCatozzi <maryuricatozzi@gmail.com>
2025-04-29 11:34:00 +0200

    Add files via upload

---
+++ spigolatura.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "https://www.focus.it/cultura/curiosita/qual-e-il-primo-blackout-della-storia"
+response = requests.get(url)
+html = response.text
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(html, "html.parser")
+
+# Trova il primo <h1>
+titolo = soup.find("h1").text
+print(titolo)
+
+# Trova tutti i paragrafi <p>
+paragrafi = soup.find_all("p")
+for p in paragrafi:
+    print(p.text)
